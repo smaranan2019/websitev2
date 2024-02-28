9cf32d6b9cff2e2b5e8a366659d40f491975d192
Frances Severino <smaranan.2019@smu.edu.sg>
2024-02-28 15:49:04 +0800

    changed index html

--- src/components/Variables.js
+++ src/components/Variables.js
@@ -1,3 +1,4 @@
 export const github = "https://github.com/smaranan2019";
 export const linkedin = "https://www.linkedin.com/in/francesseverino/";
 export const medium = "https://medium.com/@smaranan.2019";
+export const name = "Frances Severino";
