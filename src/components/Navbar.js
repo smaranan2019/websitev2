588ed22493b8fccda573f728252fd908635dd93e
Frances Severino <smaranan.2019@smu.edu.sg>
2024-03-02 22:37:00 +0800

    fixed navlink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -51,7 +51,7 @@ function NavBar() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto" defaultActiveKey="#home">
             <Nav.Item>
-              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+              <Nav.Link as={Link} to="/home" onClick={() => updateExpanded(false)}>
                 <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
               </Nav.Link>
             </Nav.Item>
